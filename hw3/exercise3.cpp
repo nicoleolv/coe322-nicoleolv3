eeaf147881ef9464715c2ea2eb581553541bbf1b
Nicole <nicoleolv@isp02.tacc.utexas.edu>
2023-09-07 14:58:39 -0500

    commiting exercise 3

---
+++ hw3/exercise3.cpp
@@ -0,0 +1,30 @@
+lude <iostream>
+
+using namespace std;
+using std::cout;
+
+int cc(int n){
+  int sequence = 0;
+
+  while (n != 1){
+    if (n % 2 == 0){
+      n = n / 2;
+    } else{
+      n = 3 * n + 1;
+    }
+    sequence++;
+  }
+  return sequence;
+  return n;
+}
+int main(){
+  int n;
+  cout << "Enter a pocitive number between from 1 to 1000: ";
+  cin >> n;
+
+  int sequence = cc(n);
+  cout << n << '\n';
+
+  return 0;
+
+}
