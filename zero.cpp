0f21e46ef251f804b14e3ea61ca7d60b098d67bc
Nicole <nicoleolv@isp02.tacc.utexas.edu>
2023-09-08 13:31:36 -0500

    hw2

---
+++ zero.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main(){
+  cout << "hello world\n"; 
+  return 0;
+
+}
+// C-x C-s : save chnages
+// C-x C-c : exit 
+// C-z : suspend
+// and then "fg" in the shell to foreground the editor
