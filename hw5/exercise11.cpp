cd6d9fb3dceaa72b2818f4c1b986190871f51776
Nicole <nicoleolv@isp02.tacc.utexas.edu>
2023-09-20 21:12:55 -0500

    exercise11

---
+++ hw5/exercise11.cpp
@@ -0,0 +1,77 @@
+// uteid: no4342
+// tacc username: nicoleolv
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool isprime(int num){ // checks to see if our input 'num' is a prime number
+  bool isprime = true;
+
+  if (num <= 1) {
+    isprime = false;
+  } else {
+      for(int divisor = 2; divisor <= num/2; divisor++){  // for loop goes through numbers to check divisibility
+	if(num % divisor == 0){
+	  isprime = false;
+	  break; // exiting the loop once a divisor is found
+	}
+      }
+
+    }
+
+  return isprime;
+}
+
+class primegenerator{
+
+public:
+  int num_primes = 0; // initialze number_of_primes_found which stores the amount of prime numbers found
+  int current = 2; // stores the current prime number which begins at 2
+
+  int number_of_primes_found() const{
+    return num_primes;
+  }
+
+  int nextprime(){ // method counts the next prime number
+    while (true) {
+      if (isprime(current)){
+	num_primes++;
+	return current++;
+      }
+      current++;
+
+    }
+  }
+
+};
+
+
+int main(){
+  int nprimes;
+  cin >> nprimes;
+
+  primegenerator sequence;
+
+  vector<int> primes;  // stores the generated 'nprimes' in a vector
+
+  while (sequence.number_of_primes_found() < nprimes){
+    int number = sequence.nextprime();
+    // cout << "Number " << number << " is prime"<< '\n';
+    primes.push_back(number);
+  }
+
+  for (int i = 0; i < primes.size(); i++){
+    int r = primes[i];
+    for (int j = i + 1; j < primes.size(); j++){
+      int q = primes[j];
+      int p = 2 * q - r;
+      if (p >= 2 && isprime(p)){
+	cout << "p: " << p << "  q: " << q << "  r: " << r << '\n';
+      } 
+    }
+  }
+
+  return 0;
+}
