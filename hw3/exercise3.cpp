f7f8b61eb8cafb62e452fa8e0cbcb753ce4f7475
Nicole <nicoleolv@isp02.tacc.utexas.edu>
2023-10-07 22:11:54 -0500

    exercises

--- hw3/exercise3.cpp
+++ hw3/exercise3.cpp
@@ -7,30 +7,30 @@
 using namespace std;
 
 
-int cc(int n){
-  int sequence = 0;
+int cc(int n){  //created a collatz conjecture function
+  int sequence = 0;  // initialized the int that will store the amount numbers in the sequence
 
-  while (n != 1){
-    if (n % 2 == 0){
+  while (n != 1){  // as long as we don't terminate to one we continue
+    if (n % 2 == 0){  // if our number n is even, divide it by two
       n = n / 2;
     } else{
-      n = 3 * n + 1;
+      n = 3 * n + 1;  // if our number is not even (odd), multiply by 3 and add one
     }
-    sequence++;
+    sequence++;  // add one to our sequence when we're done going through a number
   }
   return sequence;
 }
 
 int main(){
 
-  int maxlength = 0;
-  int startingnum = 0;
+  int maxlength = 0;  // initialzed the int that will store our maxlength of a sequence
+  int startingnum = 0;  // stores our starting number of a sequence
 
-  for (int i = 1; i <= 1000; i++){
-    int sequence = cc(i);
-    if (sequence > maxlength){
+  for (int i = 1; i <= 1000; i++){  // loops through starting numbers 1-1000 
+    int sequence = cc(i);  // calls the collatz conjecture function
+    if (sequence > maxlength){  // if our new sequence is longer than our previous 'maxlength' then our maxlength is now our sequence  
       maxlength = sequence;
-      startingnum = i;
+      startingnum = i;  
     }
   }
 
--- hw3/exercise6.cpp
+++ hw3/exercise6.cpp
@@ -7,11 +7,11 @@
 
 using namespace std;
 
-void sorter( const vector<int> &initialvec, vector<int> &oddvec, vector<int> &evenvec ){
+void sorter( const vector<int> &initialvec, vector<int> &oddvec, vector<int> &evenvec ){  // function that sorts our given vector into two vectors
   for (int num : initialvec){
-    if (num % 2 == 0){
+    if (num % 2 == 0){  // if our number is even then we add it to the even vector
       evenvec.push_back(num);
-    } else {
+    } else {  // if it is not even, we add it to our odd vector
       oddvec.push_back(num);
     }
   }
@@ -19,19 +19,19 @@ void sorter( const vector<int> &initialvec, vector<int> &oddvec, vector<int> &ev
 
 
 int main(){
-  vector<int> initialvec = {5, 6, 2, 4, 5};
+  vector<int> initialvec = {5, 6, 2, 4, 5};  // we define our vector
   vector<int> oddvec;
   vector<int> evenvec;
 
-  sorter(initialvec, oddvec, evenvec);
+  sorter(initialvec, oddvec, evenvec);  // call our sorter function
 
   cout << "Odd vector: " << endl;
-  for (int num : oddvec){
+  for (int num : oddvec){  // loops through the whole odd vector and prints out each number
     cout << num << " " << '\n';
   }
  
   cout << "Even vector: " << endl;
-  for (int num : evenvec){
+  for (int num : evenvec){  // loops through the whole even vector and prints out each number
     cout << num << " " << '\n';
   }
 
