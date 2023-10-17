9023d3e21c6154f588261cf9193d234dc0be2f33
Nicole <nicoleolv@isp02.tacc.utexas.edu>
2023-10-16 23:37:01 -0500

    exercise 4

--- hw8/compilescript.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ $# -eq 0 ] ; then
-    echo "Usage: compile_script.sh program"
-    exit
-fi
-
-icpx -o $1 $( pkg-config --cflags fmt ) $( pkg-config --libs fmt ) $1.cpp
-echo "Successfully compiled $1"
